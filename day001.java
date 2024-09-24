83875c74ea1907be363d160fb196062f8c68ad4e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-09-24 22:07:13 +0530

    Create day001.java

---
+++ day001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int[] arr=new int[5];
+      Scanner sc=new Scanner(System.in);
+      for(int i=0;i<5;i++){
+        arr[i]=sc.nextInt();
+      }
+       for(int i=4;i>=0;i--){
+       System.out.println(arr[i]);
+      }
+      
+        }
+      }
